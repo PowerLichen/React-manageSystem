890ff93b58886f8cb54e2ee30cf23265b1e99828
PowerLichen <dltm135@gmail.com>
2021-07-19 20:12:14 +0900

    Customer.js 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [{
+  'id': 1,
+  'image': 'http://placeimg.com/64/64/1',
+  'name': "김팝송",
+  'birthday': '990101',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'http://placeimg.com/64/64/2',
+  'name': "김밥",
+  'birthday': '910405',
+  'gender': '남자',
+  'job': '백수'
+},
+{
+  'id': 3,
+  'image': 'http://placeimg.com/64/64/3',
+  'name': "이백",
+  'birthday': '011205',
+  'gender': '여자',
+  'job': '교사'
+}
+]
 
 function App() {
   return (
     <div className="gray-background">
-      <img src={logo} alt="logo"/>
-      <h2>Develop Management System</h2>
+      {customers.map(c => {
+        return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+      })
+      }
     </div>
   );
 }
